f0bd4608c9162205731448c9703975cd904477fb
Mrudula Didde <mruduladidde@Mrudulas-MacBook-Pro.local>
2020-05-27 22:59:54 +0530

    Updates

---
+++ precache-manifest.097e26a69afd128401e45580d799abb8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025681bbb13623b9e8e4639dea7db056",
+    "url": "/reactMailbox/index.html"
+  },
+  {
+    "revision": "3b784c39448912985eb5",
+    "url": "/reactMailbox/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "48c2ed826dba98841d99",
+    "url": "/reactMailbox/static/css/main.b4d426cd.chunk.css"
+  },
+  {
+    "revision": "3b784c39448912985eb5",
+    "url": "/reactMailbox/static/js/2.55badfda.chunk.js"
+  },
+  {
+    "revision": "48c2ed826dba98841d99",
+    "url": "/reactMailbox/static/js/main.75b8f2ae.chunk.js"
+  },
+  {
+    "revision": "6b031ebeccba894c821e",
+    "url": "/reactMailbox/static/js/runtime-main.58cee42f.js"
+  },
+  {
+    "revision": "c7f1a998dbd248dce8371c40700c32c6",
+    "url": "/reactMailbox/static/media/icon_arrow01.c7f1a998.svg"
+  },
+  {
+    "revision": "a8497fb7c109e4c9768cbd8de8cab045",
+    "url": "/reactMailbox/static/media/icon_arrow02.a8497fb7.svg"
+  },
+  {
+    "revision": "e058455f8e2f925d5a381b6d564c8487",
+    "url": "/reactMailbox/static/media/icon_calender.e058455f.svg"
+  },
+  {
+    "revision": "fae31ac06b76f405d99afccadf6656c3",
+    "url": "/reactMailbox/static/media/icon_clip.fae31ac0.svg"
+  },
+  {
+    "revision": "b845eca9ae900345cc75533dca07b2db",
+    "url": "/reactMailbox/static/media/icon_mail_sp.b845eca9.svg"
+  },
+  {
+    "revision": "04846dcdb42b086080b88f4a4fefa4d1",
+    "url": "/reactMailbox/static/media/icon_search.04846dcd.svg"
+  }
+]);
